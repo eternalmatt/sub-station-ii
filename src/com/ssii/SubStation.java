00c8e3731bdad73cf0fa72e9e66c33f803d84f07
Matt Senn <eternalmatt@gmail.com>
2012-10-15 19:29:24 -0400

    Added sandwiches to strings.xml

--- src/com/ssii/SubStation.java
+++ src/com/ssii/SubStation.java
@@ -1,19 +1,21 @@
 package com.ssii;
 
+import roboguice.inject.ContentView;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 
-import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
+import com.github.rtyley.android.sherlock.roboguice.activity.RoboSherlockActivity;
 
-public class SubStation extends SherlockActivity {
+@ContentView(R.layout.activity_sub_station)
+public class SubStation extends RoboSherlockActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sub_station);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        
     }
     
     
